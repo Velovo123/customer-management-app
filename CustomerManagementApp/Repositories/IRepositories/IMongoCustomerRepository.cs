9e4352098f679dd17db659d5b7804e62937423ec
velovo <velovo16@gmail.com>
2024-12-09 16:25:01 +0200

    Create IRepository interface for generic CRUD operations

---
+++ CustomerManagementApp/Repositories/IRepositories/IMongoCustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerManagementApp.Repositories.IRepositories
+{
+    public interface IMongoCustomerRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+        Task<List<T>?> GetAllAsync();
+        Task<T?> GetByIdAsync(string id);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(string id);
+    }
+}
--- CustomerManagementApp/Repositories/MongoCustomerRepository.cs
+++ CustomerManagementApp/Repositories/MongoCustomerRepository.cs
@@ -4,10 +4,11 @@ using DotNetEnv;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson;
+using CustomerManagementApp.Repositories.IRepositories;
 
 namespace CustomerManagementApp.Repositories
 {
-    public class MongoCustomerRepository
+    public class MongoCustomerRepository : IRepository<Customer>
     {
         private readonly IMongoCollection<Customer> _customers;
 
@@ -22,11 +23,25 @@ namespace CustomerManagementApp.Repositories
             await _customers.InsertOneAsync(customer);
         }
 
+        public Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
         public async Task<List<Customer>?> GetAllAsync()
         {
             var customers = await _customers.Find(FilterDefinition<Customer>.Empty).ToListAsync();
             return customers.Count == 0 ? null : customers;
         }
 
+        public Task<Customer?> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
