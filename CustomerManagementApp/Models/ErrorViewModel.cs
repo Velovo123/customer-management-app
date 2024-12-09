36de43643eb9353098dc21017352b6cb5eef4600
velovo <velovo16@gmail.com>
2024-12-09 13:23:34 +0200

    Initial commit

---
+++ CustomerManagementApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CustomerManagementApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
