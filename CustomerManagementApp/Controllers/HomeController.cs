0bf3dc4cc870819deef72d73071cfa7bfbc4ecbc
velovo <velovo16@gmail.com>
2024-12-10 08:34:14 +0200

    Refactor HomeController: Redirect Index to CustomerController and remove unnecessary actions

--- CustomerManagementApp/Controllers/HomeController.cs
+++ CustomerManagementApp/Controllers/HomeController.cs
@@ -6,21 +6,10 @@ namespace CustomerManagementApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        // Home controller is mainly for routing and basic pages
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return RedirectToAction("Index", "Customer");  // Redirecting to CustomerController's Index
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
